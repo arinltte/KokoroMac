0c3113ec97593d7210cc875ef08aa20fd4b27e0d
arinltte <166289099+arinltte@users.noreply.github.com>
2026-06-05 11:22:28 +0800

    Initial Commit

---
+++ KokoroMac/KokoroMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  KokoroMacApp.swift
+//  KokoroMac
+//
+//  Created by Chen Jin Shen on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct KokoroMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
